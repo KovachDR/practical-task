bf35ebc9047d61c2c6efe3d25ea8ef74f4644937
Brein <Brein_02_93@mail.ru>
2023-04-07 15:20:26 +0300

    Practical task done

--- src/Task.java
+++ src/Task.java
@@ -1,8 +1,13 @@
 public class Task {
-
     public static void main(String[] args) {
-
+        System.out.println(numberSquared(5));
+        System.out.println(numberSquaredVersionTwo(5,2));
     }
-
+public static int numberSquared(int number){
+        return number*number;
+}
+public static double numberSquaredVersionTwo(double number, int numberPow){
+        return Math.pow(number,numberPow);
+}
 }
 
